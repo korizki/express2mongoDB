bf6e4bb41c18c89b53d82c255192dd88b8e6d2ff
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2023-02-11 07:25:49 +0800

    Update API get all user dan update user collection MongoDB

--- src/handler/phHandler.js
+++ src/handler/phHandler.js
@@ -1,8 +1,9 @@
 require("dotenv").config()
 // menggunakan function dari mongodb
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 const client = new MongoClient(process.env.uri)
 // const client = new MongoClient("mongodb+srv://ko_rizki:rizki002@safestrong.potgqke.mongodb.net/test")
+const dbuser = client.db("trial_collection").collection("dummy_data")
 
 const getAllPhData = async (req, res) => {
     // membuat array kosong
@@ -13,7 +14,7 @@ const getAllPhData = async (req, res) => {
         // select table / collection
         const table = db.collection("collection_ph")
         // get data with query
-        const cursor = table.find({cn: {$ne: "SP08"}})
+        const cursor = table.find({ cn: { $ne: "SP08" } })
         // jika data kosong eksekusi
         if (await cursor.count() == 0) {
             console.log("No record found.")
@@ -27,4 +28,29 @@ const getAllPhData = async (req, res) => {
     }
 }
 
-module.exports = { getAllPhData }
+const insertDataUser = async (req, res) => {
+    try { 
+        const result = await dbuser.insertOne(req.body)
+        res.send(`New user with id ${result.insertedId} has been added.`)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getAllUser = async (req, res) => {
+    let array = []
+    try {
+        const result = await dbuser.find()
+        await result.forEach(it => array.push(it))
+        res.send(array)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+const updateUserInfo = async (req, res) => {
+    let result = await dbuser.updateOne({_id: new ObjectId(req.query.id)}, {$set: req.body} )
+    result.modifiedCount > 0 ? res.send(`User has been updated.`) : res.send('User not found.') 
+}
+
+module.exports = { getAllPhData, insertDataUser, getAllUser, updateUserInfo }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express")
+const dotenv = require('dotenv')
 const cors = require('cors')
 const mongoose = require("mongoose")
-const { getAllPhData } = require("./handler/phHandler.js")
+const { getAllPhData, insertDataUser, getAllUser, updateUserInfo } = require("./handler/phHandler.js")
+const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(5100, () => console.log('Started at port 5100'))
-app.get('/api-ph', cors(),getAllPhData)
+const init = async () => {
+    const client = new MongoClient(process.env.URI)
+    try {
+        await client.connect()
+        console.log('Connected to MongoDB')
+    } catch(err){
+        console.log(`Error when establish connection to MongoDB`)
+    }
+}
+init()
+
+app.get('/api-ph', cors(), getAllPhData)
+app.get('/user', cors(), getAllUser)
+app.put('/user', cors(), updateUserInfo)
+app.post('/user', cors(), insertDataUser )
